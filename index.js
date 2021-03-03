14e09938107fe79ac2451ece2c7368cf6c4d7df9
kentayamauti <72152834+kentayamauti@users.noreply.github.com>
2021-03-03 21:24:09 +0900

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
- $('.faq-menu dd').hide();
-        $('.faq-menu dt').on('click', function() {
-            $(this).next().slideToggle();
-        });
-        $(document).ready(function() {
-            $('.drawer').drawer();
-        });
-    
